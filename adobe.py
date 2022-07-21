10a31ed0219c9d1f23f4ca6804a60fc127b59309
dimyasha7123 <d_galushkin@mail.ru>
2022-07-22 02:03:47 +0500

    add order config, some changes

--- adobe.py
+++ adobe.py
@@ -23,25 +23,35 @@ png_export_options.AntiAliasing = True
 png_export_options.Transparency = True
 png_export_options.ArtBoardClipping = True
 
+# создание конфига с порядком слоев
+orderList = list()
+for i in range(1, layers.Count + 1):
+    l = layers(i)
+    orderList.append({'name': l.Name.replace(' ', '-'), 'order': i})
+conf_data = {'layers': orderList}
+with open(r'order.yaml', 'w') as file:
+     outputs = yaml.dump(conf_data, file, allow_unicode=True)
+
+# экспорт слоев в пнгшки
 # layers_data = list()
-for i in range(1, layers.Count + 1):
-    l = layers(i)
-    l.Visible = False
+# for i in range(1, layers.Count + 1):
+#     l = layers(i)
+#     l.Visible = False
 
-print("All layers unvisible")
+# print("All layers unvisible")
 
-for i in range(1, layers.Count + 1):
-    l = layers(i)
-    l.Visible = True
-    # print(i, l.Name, l.Visible)
-    doc.Export(
-        ExportFile=r"C:\Users\dimya\OneDrive\Рабочий стол\seva_nft_gen\export3\{file_name}"
-        .format(file_name=l.Name),
-        ExportFormat=5,
-        Options=png_export_options
-    )
-    l.Visible = False
-    print(f"{i} Layer {l.Name} exported")
+# for i in range(1, layers.Count + 1):
+#     l = layers(i)
+#     l.Visible = True
+#     # print(i, l.Name, l.Visible)
+#     doc.Export(
+#         ExportFile=r"C:\Users\dimya\OneDrive\Рабочий стол\seva_nft_gen\export3\{file_name}"
+#         .format(file_name=l.Name),
+#         ExportFormat=5,
+#         Options=png_export_options
+#     )
+#     l.Visible = False
+#     print(f"{i} Layer {l.Name} exported")
 
 #     items = l.PathItems
 #     min_x = 9999999
--- gen.py
+++ gen.py
@@ -17,6 +17,13 @@ imageDict = dict()
 for file in fileList:
     imageDict[file[0:len(file)-4]] = Image.open(export_path + file)
 
+classes = list()
+for cl in probs['classes']:
+    classes.append(list(cl.keys())[0])
+print(classes)
+
+# сделать функцию проверки соответствия конфига и файлов
+# составляем новую генерацию пройдясь по циклам, добавляем картинки в список и сортим по порядку слоев, сливаем и так далее
 
 # l1 = Image.open("./export3/Баллончик-краски.png")
 # l2 = Image.open("./export3/Кисть-для-предмета.png")
